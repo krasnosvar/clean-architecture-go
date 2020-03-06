22ac35e6fa59a0379ecc07595ddcc768675ee224
krasnosvar <45388508+krasnosvar@users.noreply.github.com>
2020-03-06 20:57:01 +0300

    Update config_staging.go

--- config/config_staging.go
+++ config/config_staging.go
@@ -4,7 +4,7 @@ package config
 
 const (
 	MONGODB_HOST            = "mongodb://127.0.0.1:27017"
-	MONGODB_DATABASE        = "bookmark"
+	MONGODB_DATABASE        = "apidb"
 	MONGODB_CONNECTION_POOL = 5
 	API_PORT                = 8080
 )
